de8dc78173b7b61e4ec36f8543f4794711c4f411
ZulfaSalsabila <zulfa130802@gmail.com>
2022-05-22 22:16:57 +0700

    Act 9

--- app/src/main/java/com/example/aplikasisqlite/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/aplikasisqlite/adapter/TemanAdapter.java
@@ -1,31 +1,45 @@
 package com.example.aplikasisqlite.adapter;
 
+import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.aplikasisqlite.MainActivity;
 import com.example.aplikasisqlite.R;
+import com.example.aplikasisqlite.app.AppController;
 import com.example.aplikasisqlite.database.DBController;
 import com.example.aplikasisqlite.database.Teman;
 import com.example.aplikasisqlite.edit_teman;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
-    private Context control;
 
     public TemanAdapter(ArrayList<Teman> listData) {
         this.listData = listData;
@@ -35,7 +49,6 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
     public TemanAdapter.TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
         View view = layoutInf.inflate(R.layout.row_data_teman, parent, false);
-        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
@@ -46,7 +59,6 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         id = listData.get(position).getId();
         nma = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
-        DBController db = new DBController(control);
 
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setTextSize(20);
@@ -56,37 +68,85 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                PopupMenu popupMenu = new PopupMenu(control, holder.cardku);
-                popupMenu.inflate(R.menu.menu);
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                popupMenu.inflate(R.menu.popup_menu);
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem Item) {
-                        switch (Item.getItemId()){
+                        switch (Item.getItemId()) {
                             case R.id.mnEdit:
-                                Intent i = new Intent(control, edit_teman.class);
-                                i.putExtra("id",id);
-                                i.putExtra("nama",nma);
-                                i.putExtra("telpon",tlp);
-                                control.startActivity(i);
+                                Bundle bendel = new Bundle();
+                                bendel.putString("kunci1", id);
+                                bendel.putString("kunci2", nma);
+                                bendel.putString("kunci3", tlp);
+                                Intent inten = new Intent(v.getContext(), edit_teman.class);
+                                inten.putExtras(bendel);
+                                v.getContext().startActivity(inten);
                                 break;
-                            case R.id.mnHapus:
-                                HashMap<String,String> values = new HashMap<>();
-                                values.put("id",id);
-                                db.DeleteData(values);
-                                Intent j = new Intent(control, MainActivity.class);
-                                control.startActivity(j);
+                            case R.id.hapus:
+                                AlertDialog.Builder alertdb = new AlertDialog.Builder(v.getContext());
+                                alertdb.setTitle("Yakin " + nma + "akan dihapus?");
+                                alertdb.setMessage("Tekan Ya untuk menghapus");
+                                alertdb.setCancelable(false);
+                                alertdb.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int which) {
+                                        HapusData(id);
+                                        Toast.makeText(v.getContext(), "Data" + id + "telah dihapus", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(v.getContext(), MainActivity.class);
+                                        v.getContext().startActivity(intent);
+                                    }
+                                });
+                                alertdb.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                AlertDialog adlg = alertdb.create();
+                                adlg.show();
                                 break;
                         }
                         return true;
                     }
                 });
                 popupMenu.show();
-                return false;
+                return true;
             }
         });
-
     }
+    private void HapusData(final String idx) {
+        String url_update = "http://10.0.2.2/umyTI/deletetm.php";
+        final String TAG = MainActivity.class.getSimpleName();
+        final String TAG_SUCCES = "success";
+        final int[] sukses = new int[1];
 
+        StringRequest stringReq = new StringRequest(Request.Method.POST, url_update, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Respon: " + response.toString());
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    sukses[0] = jObj.getInt(TAG_SUCCES);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", idx);
+                return params;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(stringReq);
+    }
     @Override
     public int getItemCount() {
         return (listData != null) ? listData.size() : 0;
--- app/src/main/java/com/example/aplikasisqlite/edit_teman.java
+++ app/src/main/java/com/example/aplikasisqlite/edit_teman.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.aplikasisqlite.database.DBController;
 import com.google.android.material.textfield.TextInputEditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class edit_teman extends AppCompatActivity {
-    TextInputEditText Nama,Telpon;
-    Button Save;
-    String nma, tlp, id;
+    TextView idText;
+    EditText edNama, edTelpon;
+    Button editBtn;
+    String id, nma, tlp, namaEd, telponEd;
+    int sukses;
+
+    private static String url_update = "http://10.0.2.2/umyTI/updatetm.php";
+    private static final String TAG = edit_teman.class.getSimpleName();
+    private static final String TAG_SUCCES = "success";
+
     DBController controller = new DBController(this);
 
     @Override
@@ -24,39 +44,77 @@ public class edit_teman extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_teman);
 
-        Nama = findViewById(R.id.edNama);
-        Telpon = findViewById(R.id.edTelp);
-        Save = findViewById(R.id.simpanBtn);
 
-        id = getIntent().getStringExtra("id");
-        nma = getIntent().getStringExtra("nama");
-        tlp = getIntent().getStringExtra("telpon");
+        idText = findViewById(R.id.textId);
+        edNama = findViewById(R.id.edNm);
+        edTelpon=findViewById(R.id.edTlp);
+        editBtn=findViewById(R.id.buttonEdit);
 
-        setTitle("Edit Data");
-        Nama.setText(nma);
-        Telpon.setText(tlp);
+        Bundle bundle=getIntent().getExtras();
+        id=bundle.getString("kunci1");
+        nma=bundle.getString("kunci2");
+        tlp=bundle.getString("kunci3");
 
-        Save.setOnClickListener(new View.OnClickListener() {
+        idText.setText("Id:" +id);
+        edNama.setText(nma);
+        edTelpon.setText(tlp);
+
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if (Nama.getText().toString().equals("") || Telpon.getText().toString().equals("")){
-                    Toast.makeText(getApplicationContext(), "Mohon isi data terlebih dahulu !!!", Toast.LENGTH_LONG).show();
-                }else{
-                    nma = Nama.getText().toString();
-                    tlp = Telpon.getText().toString();
-                    HashMap<String,String> values = new HashMap<>();
-                    values.put("id",id);
-                    values.put("nama",nma);
-                    values.put("telpon",tlp);
-                    controller.UpdateData(values);
-                    callHome();
-                }
+            public void onClick(View v) {
+                EditData();
             }
         });
     }
-    private void callHome(){
-        Intent i = new Intent(edit_teman.this,MainActivity.class);
-        startActivity(i);
+    public void EditData() {
+        namaEd = edNama.getText().toString();
+        telponEd = edTelpon.getText().toString();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringReq = new StringRequest(Request.Method.POST, url_update, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Respon: " + response.toString());
+                try {
+                    JSONObject jobj = new JSONObject(response);
+                    sukses = jobj.getInt(TAG_SUCCES);
+                    if (sukses == 1) {
+                        Toast.makeText(edit_teman.this, "Sukses mengedit data", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(edit_teman.this, "gagal", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error : " + error.getMessage());
+                Toast.makeText(edit_teman.this, "Gagal Edit data", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("id", id);
+                params.put("nama", namaEd);
+                params.put("telpon", telponEd);
+
+                return params;
+            }
+        };
+        requestQueue.add(stringReq);
+        CallHomeActivity();
+    }
+    public void CallHomeActivity()
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
         finish();
+
     }
 }
